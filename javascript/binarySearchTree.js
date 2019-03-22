9c344067fb836ad2cae6ec045bd5d1b25c74179c
vitaliyvorobyov <vorobyov@ucu.edu.ua>
2019-03-22 19:11:46 +0200

    Rewritten BST

--- javascript/binarySearchTree.js
+++ javascript/binarySearchTree.js
@@ -1,80 +1,81 @@
+"use strict";
+
 class BinarySearchTree {
 
-    constructor(data) {
+    constructor(data = null) {
+        this.data = data;
         this.left = null;
         this.right = null;
-        this.data = data;
     }
 
-    addItem(item) {
-
+    insert(data) {
         if (this.data) {
-            if (item < this.data) {
-                if (this.left == null) {
-                    this.left = new BinarySearchTree(item);
+            if (data < this.data) {
+                if (this.left === null) {
+                    this.left = new BinarySearchTree(data);
                 } else {
-                    this.left.addItem(item);
+                    this.left.insert(data);
                 }
-            } else if (item > this.data) {
-                if (this.right == null) {
-                    this.right = new BinarySearchTree(item);
+            } else if (data > this.data) {
+                if (this.right === null) {
+                    this.right = new BinarySearchTree(data);
                 } else {
-                    this.right.addItem(item);
+                    this.right.insert(data);
                 }
             }
         } else {
-            this.data = item;
+            this.data = data;
         }
-
     }
 
-    searchItem(item) {
-
-        if (item < this.data) {
-            if (this.left == null) {
+    search(data) {
+        if (data < this.data) {
+            if (this.left === null) {
+                console.log( "Nothing is found");
                 return false;
             }
-            return this.left.searchItem(item);
-        } else if (item > this.data) {
-            if (this.right == null) {
+            return this.left.search(data);
+        } else if (data > this.data) {
+            if (this.right === null) {
+                console.log( "Nothing is found");
                 return false;
             }
-            return this.right.searchItem(item);
+            return this.right.search(data);
         } else {
+            console.log("Item is found");
             return true;
         }
-
     }
 
-    // TODO: write item deletion
+    print() {
 
-    deleteItem(item) {
-
-    }
-
-    printTree() {
-
-        console.log(this.data);
-
-        if (this.left != null) {
-            this.left.printTree();
+        function recurse(node, level) {
+            let s = "";
+            if (node !== null) {
+                s += recurse(node.right, level + 1);
+                s += "| ".repeat(level);
+                s += node.data + "\n";
+                s += recurse(node.left, level + 1);
+            }
+            return s;
         }
 
-        if (this.right != null) {
-            this.right.printTree();
-        }
+        console.log(recurse(this, 0));
 
     }
 
 }
 
-let SBT = new BinarySearchTree(5);
+let bst = new BinarySearchTree(6);
 
-SBT.addItem(3);
-SBT.addItem(0);
-SBT.addItem(0);
-SBT.addItem(6);
-SBT.addItem(43);
-SBT.addItem(2);
+bst.insert(5);
+bst.insert(2);
+bst.insert(8);
+bst.insert(7);
+bst.insert(6);
+bst.insert(0);
+bst.insert(23);
 
-SBT.printTree();
+bst.search(23);
+
+bst.print();
