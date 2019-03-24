30530ef3ad3145cb3f06a3f1d21ec9e6a1f4845e
andriiaprysiazhnyk <prysiazhnyk@ucu.edu.ua>
2019-03-24 14:16:37 +0200

    Radix sort implementation.

--- Andrii_Prysiazhnyk/algorithms/sorting/radixSort.js
+++ Andrii_Prysiazhnyk/algorithms/sorting/radixSort.js
@@ -1,6 +1,34 @@
 // Works only for array of non negative numbers.
 function radixSort(array) {
+    let maxElement = getMaxElement(array), divider = 1;
 
+    while (maxElement / divider ^ 0 > 0) {
+        countingSortByOneDigit(array, divider);
+        divider *= 10;
+    }
+}
+
+function countingSortByOneDigit(array, divider) {
+    let countingArray = new Array(10), sortedArray = new Array(array.length);
+    countingArray.fill(0);
+
+    for (let i = 0; i < array.length; ++i) {
+        countingArray[(array[i] / divider ^ 0) % 10]++;
+    }
+
+    for (let i = 1; i < 10; ++i) {
+        countingArray[i] += countingArray[i - 1];
+    }
+
+    let digit;
+    for (let i = array.length - 1; i >= 0; --i) {
+        digit = (array[i] / divider ^ 0) % 10;
+        sortedArray[ --countingArray[digit] ] = array[i];
+    }
+
+    for (let i = 0; i < array.length; ++i) {
+        array[i] = sortedArray[i];
+    }
 }
 
 function getMaxElement(array) {
@@ -14,5 +42,3 @@ function getMaxElement(array) {
 
     return result;
 }
-
-console.log(123 / 100);
