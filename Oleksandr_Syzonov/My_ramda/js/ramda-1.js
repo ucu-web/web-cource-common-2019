62689fca7c348b2bacd1464df60da00ddc68b67c
Arattel <sizonov2000@gmail.com>
2019-04-09 14:52:21 +0300

    Added function none

--- Oleksandr_Syzonov/My_ramda/js/ramda-1.js
+++ Oleksandr_Syzonov/My_ramda/js/ramda-1.js
@@ -5,3 +5,10 @@ let isEmpty = (a) => (a[0] === undefined);
 let head = (a) => (isEmpty(a) ? undefined : a[0]);
 let not = (a) => (!a);
 let negate = (a) => (-a);
+function filter(arr, func, index = 0){
+    return index === arr.length ? [] : func(arr[index]) ? [arr[index], ...filter(arr, func, index + 1)] : filter(arr, func, index + 1);
+}
+function map(arr, func, index = 0) {
+    return index === arr.length ? [] : [func(arr[index]), ...map(arr,  func, index + 1)]
+}
+let none = (func, a) => (isEmpty(filter(a, func)));
