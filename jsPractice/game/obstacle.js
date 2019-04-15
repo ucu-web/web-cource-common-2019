06ba8ec627d8099788722f8942b33be6c0e52953
Sophia K <kholod@ucu.edu.ua>
2019-04-15 21:43:50 +0300

    add obstacle.js, dino.js

---
+++ jsPractice/game/obstacle.js
@@ -0,0 +1,34 @@
+class Obstacle{
+    constructor(x, y, type) {
+        this.type = type;
+        this.img = new Image();
+        switch (this.type) {
+            case 0:  // small cactus
+                this.width = 20;
+                this.height = 40;
+                this.img.src = "img/cactusSmall0000.png";
+                break;
+            case 1:  // big cactus
+                this.width = 30;
+                this.height = 60;
+                this.img.src = "img/cactusBig0000.png";
+                break;
+            case 2:  // many small
+                this.width = 60;
+                this.height = 40;
+                this.img.src = "img/cactusSmallMany0000.png";
+                break;
+        }
+        this.x = x - this.width;
+        this.y = y - this.height;
+    }
+    update(){
+        let ctx = game_area.context;
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    new_pos(y){
+        this.x -= y*speed;
+    }
+
+}
