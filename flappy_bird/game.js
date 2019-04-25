f6afece9f71dec901c3a386dbcff1a25928e3f0d
vitaliyvorobyov <vorobyov@ucu.edu.ua>
2019-04-25 18:30:27 +0300

    Big commit

--- flappy_bird/game.js
+++ flappy_bird/game.js
@@ -1,25 +1,73 @@
 class Bird {
-    constructor(width, height, weight) {
-        this.width = width;
-        this.height = height;
+    constructor(x, y, w, h, weight, context, canvas) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
         this.weight = weight;
+        this.ctx = context;
+        this.cv = canvas;
+        window.addEventListener("keyup", (event) => this.update(event.key), false);
     }
 
-    colide() {
+    draw(x = this.x, y = this.y) {
+        this.ctx.fillStyle = "#2ecc71";
+        this.ctx.fillRect(x, y, this.width, this.height);
+    }
 
+    gravity() {
+        console.log("TT: ", this.cv.height, this.y + this.weight);
+        if (this.y + this.weight + this.height < this.cv.height) {
+            this.y = this.y + this.weight;
+        }
+    }
+
+    fly() {
+        this.y = this.y - 20;
+    }
+
+    update(key) {
+        this.gravity(this.ctx, this.cv.width, this.cv.height);
+        this.draw(this.x, this.y);
+
+        if (key === " ") {
+            this.y = this.y - 100;
+        }
     }
 }
 
 class Pipe {
+    constructor() {
+        this.x =
+    }
 
+    draw(ctx, gap, x, y, w, h) {
+        this.pipeNorth.onload = () => ctx.drawImage(this.pipeNorth, x, y, w, h);
+        this.pipeSouth.onload = () => ctx.drawImage(this.pipeSouth, x, y + gap + this.pipeNorth.height, w, h);
+    }
 }
 
 class Game {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
+    constructor() {
+        this.canvas = document.getElementById("game");
+        this.context = this.canvas.getContext("2d");
+        this.canvas.background = "#2980b9";
+        this.bird = new Bird(50, 50, 50, 50, 1, this.context, this.canvas);
     }
 
+    start() {
+        this.bird.draw();
 
+        setInterval(
+            () => {
+                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                this.bird.update();
+            },
+            4
+        )
+    }
 }
 
+
+let game = new Game();
+game.start();
---
+++ game-of-life/game.js
@@ -0,0 +1,33 @@
+FIELD_SIZE = 100;
+
+const generateRow = (size) => new Array(size).fill(size).map(() => Math.round(Math.random()));
+const generateField = (size) => new Array(size).fill(size).map(() => generateRow(size));
+const getCellValue = (field, i, j) => !field[i] ? 0 : !field[i][j] ? 0 : field[i][j];
+const updateField = field => field.map((row, i) => row.map((cell, j) => getNewCellStatus(field, i, j)));
+
+
+const getNewCellStatus = (field, i, j) => getCellValue(field, i, j) ? sumOfNeighbors(field, i, j) < 2 ? 0 : sumOfNeighbors(field, i, j) > 3 ? 0 : 1 : sumOfNeighbors(field, i, j) === 3 ? 1 : 0 ;
+
+const renderView = field =>
+    field.map((row) =>
+        row.map((cell) =>
+            `<div class="Cell Cell--${cell ? "live" : "dead"}"></div>`).join("")).join("");
+
+const updateView = (elementString) => document.getElementById("game").innerHTML = elementString;
+
+const sumOfNeighbors = (field, i, j) =>
+    getCellValue(field, i - 1, j - 1) +
+    getCellValue(field, i - 1, j) +
+    getCellValue(field, i - 1, j + 1) +
+    getCellValue(field, i, j - 1) +
+    getCellValue(field, i, j + 1) +
+    getCellValue(field, i + 1, j - 1) +
+    getCellValue(field, i + 1, j) +
+    getCellValue(field, i + 1, j + 1);
+
+let field = generateField(50);
+
+setInterval(() => {
+    updateView(renderView(field));
+    field = updateField(field);
+}, 50);
