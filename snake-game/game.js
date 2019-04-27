455408ab6144b7b7df99bf3edc438667a21662af
vitaliyvorobyov <vorobyov@ucu.edu.ua>
2019-04-27 21:16:41 +0300

    Updated Snake Game

--- snake-game/game.js
+++ snake-game/game.js
@@ -1,18 +1,24 @@
-let canvas = document.getElementById("game");
-let context = canvas.getContext("2d");
+const CANVAS_SIZE = 500;
+const SNAKE_SIZE = 10;
+const SNAKE_SPEED = 100;
 
-getRandomNumber = () => Math.floor(Math.random() * 50) * 10;
+let canvas = document.getElementById("game");
+canvas.height = CANVAS_SIZE;
+canvas.width = CANVAS_SIZE;
+let context = canvas.getContext("2d");
+let score = document.getElementsByClassName("score")[0];
 
 class Snake {
-    constructor(x, y, width, height) {
+    constructor(x, y, snakeSize, canvasSize) {
         this.x = x;
         this.y = y;
         this.prevX = 0;
         this.prevY = 0;
         this.snake = [];
         this.score = 1;
-        this.width = width;
-        this.height = height;
+        this.width = snakeSize;
+        this.height = snakeSize;
+        this.canvasSize = canvasSize;
         this.verDirection = 0;
         this.horDirection = 1;
     }
@@ -29,8 +35,8 @@ class Snake {
         return false;
     }
 
-    eat(foodX, foodY) {
-        if (foodX === this.x && foodY === this.y) {
+    eat(food) {
+        if (food.x === this.x && food.y === this.y) {
             food.isEaten = true;
             this.score++;
             this.snake.push([this.x, this.y]);
@@ -51,15 +57,15 @@ class Snake {
         this.prevX = this.x;
         this.prevY = this.y;
 
-        this.x = (this.x + (this.width * this.horDirection)) % 500;
-        this.y = (this.y + (this.height * this.verDirection)) % 500;
+        this.x = (this.x + (this.width * this.horDirection)) % this.canvasSize;
+        this.y = (this.y + (this.height * this.verDirection)) % this.canvasSize;
 
         if (this.x < 0) {
-            this.x = 500;
+            this.x = this.canvasSize;
         }
 
         if (this.y < 0) {
-            this.y = 500;
+            this.y = this.canvasSize;
         }
 
         if (this.snake.length > 0) {
@@ -83,82 +89,111 @@ class Snake {
 }
 
 class Food {
-    constructor(x, y) {
+    constructor(x, y, size) {
         this.x = x;
         this.y = y;
+        this.width = size;
+        this.height = size;
         this.isEaten = false;
     }
 
     render() {
         context.fillStyle = "rgb(255, 210, 0)";
-        context.fillRect(this.x, this.y, 10, 10);
+        context.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
-// INITIALIZING GAME OBJECTS
+class Game {
+    constructor(canvasSize, snakeSize, gameSpeed) {
+        this.canvasWidth = canvasSize;
+        this.canvasHeight = canvasSize;
+        this.snakeSize = snakeSize;
+        this.speed = gameSpeed;
+        this.snake = new Snake(
+            Math.floor(this.canvasWidth / 2),
+            Math.floor(this.canvasHeight / 2),
+            this.snakeSize,
+            this.canvasWidth
+        );
+        this.food = new Food(this.getRandomNumber(), this.getRandomNumber(), this.snakeSize);
 
-const snake = new Snake(50, 50, 10, 10);
-let food = new Food(getRandomNumber(), getRandomNumber());
+        this.touchstartX = 0;
+        this.touchstartY = 0;
+        this.deltaX = 0;
+        this.deltaY = 0;
 
-// CONTROL FOR TOUCHSCREEN
-
-let touchstartX = 0;
-let touchstartY = 0;
-let deltaX = 0;
-let deltaY = 0;
-
-document.addEventListener("touchstart", (event) => {
-    touchstartX = event.touches[0].clientX;
-    touchstartY = event.touches[0].clientY;
-});
-
-document.addEventListener("touchend", (event) => {
-    deltaX = event.changedTouches[0].clientX - touchstartX;
-    deltaY = event.changedTouches[0].clientY - touchstartY;
-    handleGesture();
-});
-
-function handleGesture() {
-    if (deltaX > 0 && Math.abs(deltaY) < Math.abs(deltaX)) {
-        snake.changeDirection(0, 1);
-    } else if (deltaX < 0 && Math.abs(deltaY) < Math.abs(deltaX)) {
-        snake.changeDirection(0, -1);
-    } else if (deltaY > 0 && Math.abs(deltaX) < Math.abs(deltaY)) {
-        snake.changeDirection(1, 0);
-    } else if (deltaY < 0 && Math.abs(deltaX) < Math.abs(deltaY)) {
-        snake.changeDirection(-1, 0);
+        this.initializeControl();
     }
-}
 
-// CONTROL FOR KEYBOARD
-
-document.addEventListener("keypress", (event) => {
-    if (event.key === "w") {
-        snake.changeDirection(-1, 0);
-    } else if (event.key === "s") {
-        snake.changeDirection(1, 0);
-    } else if (event.key === "a") {
-        snake.changeDirection(0, -1);
-    } else if (event.key === "d") {
-        snake.changeDirection(0, 1);
+    getRandomNumber() {
+        return Math.floor(Math.random() * this.canvasWidth / this.snakeSize) * this.snakeSize;
     }
-});
 
-// MAIN LOOP
+    initializeControl() {
 
-setInterval(
-    () => {
-        context.fillStyle = "rgb(90, 90, 90)";
-        context.fillRect(0, 0, 500, 500);
+        // CONTROL FOR TOUCHSCREEN
 
-        snake.update();
-        snake.render();
-        snake.isDead();
-        food.render();
+        document.addEventListener("touchstart", (event) => {
+            this.touchstartX = event.touches[0].clientX;
+            this.touchstartY = event.touches[0].clientY;
+        });
 
-        snake.eat(food.x, food.y);
-        if (food.isEaten) {
-            food = new Food(getRandomNumber(), Math.floor(getRandomNumber()));
+        document.addEventListener("touchend", (event) => {
+            this.deltaX = event.changedTouches[0].clientX - this.touchstartX;
+            this.deltaY = event.changedTouches[0].clientY - this.touchstartY;
+            handleGesture();
+        });
+
+        function handleGesture() {
+            if (this.deltaX > 0 && Math.abs(this.deltaY) < Math.abs(this.deltaX)) {
+                this.snake.changeDirection(0, 1);
+            } else if (this.deltaX < 0 && Math.abs(this.deltaY) < Math.abs(this.deltaX)) {
+                this.snake.changeDirection(0, -1);
+            } else if (this.deltaY > 0 && Math.abs(this.deltaX) < Math.abs(this.deltaY)) {
+                this.snake.changeDirection(1, 0);
+            } else if (this.deltaY < 0 && Math.abs(this.deltaX) < Math.abs(this.deltaY)) {
+                this.snake.changeDirection(-1, 0);
+            }
         }
-    }, 200
-);
+
+        // CONTROL FOR KEYBOARD
+
+        document.addEventListener("keypress", (event) => {
+            if (event.key === "w") {
+                this.snake.changeDirection(-1, 0);
+            } else if (event.key === "s") {
+                this.snake.changeDirection(1, 0);
+            } else if (event.key === "a") {
+                this.snake.changeDirection(0, -1);
+            } else if (event.key === "d") {
+                this.snake.changeDirection(0, 1);
+            }
+        });
+    }
+
+    play() {
+        setInterval(
+            () => {
+                context.fillStyle = "rgb(90, 90, 90)";
+                context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+                this.food.render();
+                this.snake.update();
+                this.snake.render();
+                this.snake.isDead();
+
+                context.fillStyle = "rgb(255, 210, 0)";
+                context.font = "1em PT Mono";
+                context.fillText(`Score: ${this.snake.score}`, 10, 25);
+
+                this.snake.eat(this.food);
+                if (this.food.isEaten) {
+                    this.food = new Food(this.getRandomNumber(), this.getRandomNumber(), this.snakeSize);
+                }
+            }, this.speed
+        );
+    }
+}
+
+let game = new Game(CANVAS_SIZE, SNAKE_SIZE, SNAKE_SPEED);
+game.play();
