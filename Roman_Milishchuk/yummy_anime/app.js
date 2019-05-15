32a20a950414807ac60b62dd64b5f8a20cb66e82
Roman Milishchuk <milromchuk@gmail.com>
2019-05-15 21:16:18 +0300

    Added server for html rendering

---
+++ Roman_Milishchuk/yummy_anime/app.js
@@ -0,0 +1,19 @@
+let http = require('http');
+let fs = require('fs');
+
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, (error, data) => {
+        if (error) {
+            response.writeHead(404);
+            respone.write('file not found');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+
+http.createServer(handleRequest).listen(8000);
