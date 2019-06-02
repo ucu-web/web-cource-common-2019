9e6ae79d2d28bf3a50282e2ebd4cfac5ea76b6d1
andriiaprysiazhnyk <prysiazhnyk@ucu.edu.ua>
2019-06-02 11:07:15 +0300

    Bezier curves v2

--- Andrii_Prysiazhnyk/bezier_curves/src/bezierCurve.js
+++ Andrii_Prysiazhnyk/bezier_curves/src/bezierCurve.js
@@ -1,18 +1,20 @@
 const d3 = require("d3");
 const {multiplyPointByScalar, addPoints, combination} = require("./myMath.js");
 
-const bezierPath = (points) => {
-    let path = [], t = 0, delta = 0.005;
+const renderBezierCurve = (points, container) => {
+    const path = bezierPath(points), line = d3.line();
 
-    while (t <= 1) {
-        path.push(getPointOnBezierCurve(points, t));
-        t += delta;
-    }
-
-    path.push(points[points.length - 1]);
-
-    return path;
+    container.select(".Bezier-Visualization__curve")
+        .attr("d", line([path[0]]))
+        .transition()
+        .duration(points.length * 1000)
+        .attrTween("d", () => (t) => line(path.slice(0, Math.ceil(path.length * t))));
 };
+
+const bezierPath = (points) =>
+    new Array(points.length * 200)
+        .fill(0)
+        .map((_, i) => getPointOnBezierCurve(points, i / (points.length * 200)));
 
 const getPointOnBezierCurve = (points, t) => {
     return points
@@ -22,4 +24,4 @@ const getPointOnBezierCurve = (points, t) => {
 
 const bezierCoefficient = (i, n, t) => combination(n, i) * Math.pow(t, i) * Math.pow(1 - t, n - i);
 
-module.exports.bezierPath = bezierPath;
+module.exports.renderBezierCurve = renderBezierCurve;
--- Andrii_Prysiazhnyk/bezier_curves/src/index.js
+++ Andrii_Prysiazhnyk/bezier_curves/src/index.js
@@ -1,24 +1,5 @@
 const d3 = require("d3");
-const {bezierPath} = require("./bezierCurve.js");
-
-const renderBezierCurve = (points, container) => {
-    const path = bezierPath(points), line = d3.line();
-
-    container.select(".Bezier-Visualization__curve")
-        .attr("d", line([path[0]]))
-        .transition()
-        .duration(3000)
-        .attrTween("d", () => (t) => line(path.slice(1, Math.ceil(path.length * t))));
-
-    container.select(".Bezier-Visualization__pivots")
-        .attr("d", line(points.slice(0, -1)))
-        .transition()
-        .duration(3000)
-        .attr("d", line(points.slice(1)));
-
-    container.select(".Bezier-Visualization__line")
-        .attr("d", line(points));
-};
+const {renderBezierCurve} = require("./bezierCurve.js");
 
 const renderPoint = (point, container) => {
     container
@@ -27,6 +8,18 @@ const renderPoint = (point, container) => {
         .attr("cy", point[1])
         .attr("r", 5);
 };
+
+const renderPivotsLine = (points, container) => {
+    container.select(".Bezier-Visualization__pivots")
+        .attr("d", d3.line()(points.slice(0, -1)))
+        .interrupt()
+        .transition()
+        .duration(points.length * 1000)
+        .attr("d", d3.line()(points.slice(1)));
+};
+
+const renderStationaryLine = (points, container) => container.select(".Bezier-Visualization__stationary-line")
+    .attr("d", d3.line()(points));
 
 const start = () => {
     const width = 800, height = 500;
@@ -38,57 +31,20 @@ const start = () => {
 
     field.append("path").attr("class", "Bezier-Visualization__curve");
     field.append("path").attr("class", "Bezier-Visualization__pivots");
-    field.append("path").attr("class", "Bezier-Visualization__line");
+    field.append("path").attr("class", "Bezier-Visualization__stationary-line");
 
     field.on("click", function () {
         const newPoint = d3.mouse(this);
         renderPoint(newPoint, field);
 
         points.push(newPoint);
-        renderBezierCurve(points, field);
+
+        if (points.length > 1) {
+            renderStationaryLine(points, field);
+            renderBezierCurve(points, field);
+            renderPivotsLine(points, field);
+        }
     });
 };
 
 start();
-
-// var data = [1,9,4,2,6,2,5,0,5,9,7,2].map(function(d,i) {
-//     return [i, d];
-// });
-//
-// var w = 300,
-//     h = 100;
-//
-// var x = d3.scaleLinear()
-//     .domain([0, data.length-1])
-//     .range([0, w]);
-//
-// var y = d3.scaleLinear()
-//     .domain([0, 10])
-//     .range([h, 0]);
-//
-// var line = d3.line()
-//     .x(function(d) { return x(d[0]); })
-//     .y(function(d) { return y(d[1]); });
-//
-// var svg = d3.select('body').append('svg')
-//     .attr('w', w)
-//     .attr('h', h);
-//
-// // add element and transition in
-// var path = svg.append('path')
-//     .attr('class', 'Bezier-Visualization__curve')
-//     .attr('d', line(data[0]))
-//     .transition()
-//     .duration(3000)
-//     .attrTween('d', pathTween);
-//
-// function pathTween() {
-//     var interpolate = d3.scaleQuantile()
-//         .domain([0,1])
-//         .range(d3.range(1, data.length + 1));
-//     return function(t) {
-//         return line(data.slice(0, interpolate(t)));
-//         // console.log(Math.ceil(t * data.length));
-//         // return line(data.slice(0, Math.ceil(t * data.length)));
-//     };
-// }
