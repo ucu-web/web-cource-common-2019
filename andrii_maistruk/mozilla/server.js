83be250d6658a6d1f0378fecabe7bf0945adb1b1
kwh44 <maistruk44@gmail.com>
2019-06-03 00:11:49 +0300

    added: improved data sending

--- andrii_maistruk/mozilla/server.js
+++ andrii_maistruk/mozilla/server.js
@@ -5,33 +5,35 @@ let server = new http.Server();
 
 server.listen(5000, "127.0.0.1");
 
-let data_index = fs.readFileSync('index.html', 'utf8');
-
-server.on("request", function (req, response) {
+server.on("request", function (req, res) {
     if (req.url === "/") {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data_index);
-        response.end();
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        let file = new fs.ReadStream('index.html');
+        sendFile(file, res);
     } else {
         let path = req.url.substr(1, req.url.length);
         fs.exists(path, (exists) => {
             if (exists) {
-                fs.readFile(path, function (error, data) {
-                    if (error) {
-                        response.writeHead(404);
-                        response.write(error);
-                        response.end();
-                    } else {
-                        response.writeHead(200);
-                        response.write(data);
-                        response.end();
-                    }
-                })
+                let file = new fs.ReadStream(path);
+                sendFile(file, res);
             } else {
-                response.writeHead(404);
-                response.write("404: NOT FOUND");
-                response.end();
+                res.writeHead(404);
+                res.write("404: NOT FOUND");
+                res.end();
             }
         })
     }
 });
+
+function sendFile(file, res) {
+    file.pipe(res);
+
+    file.on('error', function (err) {
+        console.err(err);
+        file.destroy();
+    });
+
+    res.on('close', function () {
+        file.destroy();
+    });
+}
