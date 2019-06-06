7c067327076c30d41c1a2cdfc3231a0846865a96
mykhailoivankiv <neformal.lviv@gmail.com>
2019-06-06 18:01:04 +0300

    Update babel config. Simplify some methods.

--- Andriy_Dmytruk/doodleJump/webpack.config.js
+++ Andriy_Dmytruk/doodleJump/webpack.config.js
@@ -1,21 +1,29 @@
-const path = require('path');
-
+const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./static/index.js",
-    output: {
-        path: path.resolve("", 'dist'),
-        filename: "bundle.js"
-    }
+  mode: "development",
+  entry: "./static/index.js",
+  output: {
+    path: path.resolve("", "dist"),
+    filename: "bundle.js"
+  },
 
-    ,
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            },
-        ]
-    }
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
 };
