3b83b9fc8f1f72c1b41ae7d7468fda0f979e1ffc
kwh44 <maistruk44@gmail.com>
2019-06-07 16:57:50 +0300

    making things good part three

--- andrii_maistruk/mozilla/server.js
+++ andrii_maistruk/mozilla/server.js
@@ -14,10 +14,27 @@ server.on("request", function (req, res) {
         let path = req.url.substr(1, req.url.length);
         fs.exists(path, (exists) => {
             if (exists) {
+                if (path.endsWith("js")) {
+                    res.writeHead(200, {'Content-Type': 'text/javascript'});
+                }
+                if (path.endsWith("css")) {
+                    res.writeHead(200, {'Content-Type': 'text/css'});
+                }
+                if (path.endsWith("png")) {
+                    res.writeHead(200, {'Content-Type': 'image/png'});
+                }
+                if (path.endsWith("jpeg") || path.endsWith("jpg")) {
+                    res.writeHead(200, {'Content-Type': 'image/jpeg'});
+                }
+                if (path.endsWith("svg")) {
+                    res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+                }
+
                 let file = new fs.ReadStream(path);
                 sendFile(file, res);
             } else {
                 res.writeHead(404);
+
                 res.write("404: NOT FOUND");
                 res.end();
             }
---
+++ andrii_maistruk/mozilla/static/js/form.js
@@ -0,0 +1,11 @@
+
+
+function show_details() {
+    let email = document.getElementById("id_email");
+    let details = document.getElementById("sign_up_details");
+    if (email.value.toString().length >= 4) {
+        details.style.visibility = "visible";
+        details.style.display = "block";
+    } else {
+    }
+}
