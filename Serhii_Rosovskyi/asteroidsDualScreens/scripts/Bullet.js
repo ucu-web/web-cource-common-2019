e9ef9727744d1683c60fe86aea40477f635f7dc8
Serhii Rosovskyi <30779010+Rosovskyy@users.noreply.github.com>
2019-06-09 21:41:20 +0300

    Project restructured and refactored.

--- Serhii_Rosovskyi/asteroidsDualScreens/scripts/Bullet.js
+++ Serhii_Rosovskyi/asteroidsDualScreens/scripts/Bullet.js
@@ -1,25 +1,24 @@
-class Bullet {
-  constructor(ctx, x, y, a) {
-    this.ctx = ctx;
-    this.x = x;
-    this.y = y;
-    this.lambda = Math.cos(a) * 10;
-    this.beta = -Math.sin(a) * 10;
-  }
+export default class Bullet {
+    constructor(ctx, x, y, vectorVelocity, size) {
+        this.ctx = ctx;
+        this.x = x;
+        this.y = y;
+        this.xVelocity = Math.cos(vectorVelocity) * 10;
+        this.yVelocity = -Math.sin(vectorVelocity) * 10;
+        this.size = size;
+    }
 
-  draw() {
-    this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, 4, 0, Math.PI * 2);
-    this.ctx.strokeStyle = "red";
-    this.ctx.fillStyle = "white";
-    this.ctx.fill();
-  }
+    draw() {
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        this.ctx.strokeStyle = "red";
+        this.ctx.fillStyle = 'white';
+        this.ctx.fill();
+    }
 
-  update() {
-    this.x += this.lambda;
-    this.y += this.beta;
-    this.draw();
-  }
+    update() {
+        this.x += this.xVelocity;
+        this.y += this.yVelocity;
+        this.draw();
+    }
 }
-
-export default Bullet ;
--- Serhii_Rosovskyi/asteroidsDualScreens/scripts/SpaceShip.js
+++ Serhii_Rosovskyi/asteroidsDualScreens/scripts/SpaceShip.js
@@ -1,119 +1,118 @@
 import Bullet from "./Bullet.js";
 
-class SpaceShip {
-  constructor(ctx, canvas, x, y, color) {
-    this.ctx = ctx;
-    this.canvas = canvas;
-    this.x = x;
-    this.y = y;
-    this.size = 20;
-    this.a = (90 / 180) * Math.PI;
-    this.radius = this.size / 2;
-    this.lastFrame = 0;
-    this.dt = 0;
-    this.turnSpeed = 2 * Math.PI;
-    this.rotCoef = 0;
 
-    this.moveSpeed = 5;
-    this.movePower = { x: 0, y: 0 };
-    this.moveing = false;
+export default class SpaceShip {
 
-    this.timeLastFired = 0;
-    this.fireRate = 2;
-    this.bullets = [];
+    constructor(ctx, canvas, x, y, color) {
+        this.ctx = ctx;
+        this.canvas = canvas;
+        this.x = x;
+        this.y = y;
+        this.size = 20;
+        this.angle = 90 / 180 * Math.PI;
+        this.radius = this.size / 2;
+        this.lastFrame = 0;
+        this.dt = 0;
+        this.turnSpeed = 2 * Math.PI;
+        this.rotCoef = 0;
 
-    this.color = color;
+        this.moveSpeed = 5;
+        this.movePower = {x: 0, y: 0};
+        this.moveing = false;
 
-    this.fireSound = new Audio("audio/shoot.wav");
-    this.moveSound = new Audio("audio/jetpack.wav");
-    this.deathSound = new Audio("audio/lose.wav");
+        this.timeLastFired = 0;
+        this.fireRate = 2;
+        this.bullets = [];
+        this.bulletSize = 4;
 
-    console.log(this);
-  }
+        this.color = color;
 
-  rotate() {
-    this.a += this.rotCoef * this.turnSpeed * this.dt;
-  }
+        this.fireSound = new Audio("audio/shoot.wav");
+        this.moveSound = new Audio("audio/jetpack.wav");
+        this.deathSound = new Audio("audio/lose.wav");
 
-  draw() {
-    this.ctx.strokeStyle = "black";
-    this.ctx.fillStyle = this.color;
-    this.ctx.lineWidth = this.size / 20;
-    this.ctx.beginPath();
-    this.ctx.moveTo(
-      this.x + (4 / 3) * this.radius * Math.cos(this.a),
-      this.y - (4 / 3) * this.radius * Math.sin(this.a)
-    );
-    this.ctx.lineTo(
-      this.x - this.radius * ((2 / 3) * Math.cos(this.a) + Math.sin(this.a)),
-      this.y + this.radius * ((2 / 3) * Math.sin(this.a) - Math.cos(this.a))
-    );
-    this.ctx.lineTo(
-      this.x - this.radius * ((2 / 3) * Math.cos(this.a) - Math.sin(this.a)),
-      this.y + this.radius * ((2 / 3) * Math.sin(this.a) + Math.cos(this.a))
-    );
+        console.log(this);
 
-    this.ctx.closePath();
-    this.ctx.stroke();
-    this.ctx.fill();
-  }
-
-  move() {
-    this.movePower.x += this.moveSpeed * Math.cos(this.a) * this.dt;
-    this.movePower.y -= this.moveSpeed * Math.sin(this.a) * this.dt;
-    this.x += this.movePower.x;
-    this.y += this.movePower.y;
-    this.moveSound.play();
-  }
-
-  decelerate() {
-    let friction = 0.5;
-    this.movePower.x -= friction * this.movePower.x * this.dt;
-    this.movePower.y -= friction * this.movePower.y * this.dt;
-    this.x += this.movePower.x;
-    this.y += this.movePower.y;
-  }
-
-  fire(time) {
-    console.log("fire");
-    if (time - this.timeLastFired > this.fireRate) {
-      this.bullets.push(new Bullet(this.ctx, this.x, this.y, this.a));
-      this.fireSound.play();
-      this.timeLastFired = time;
     }
-  }
 
-  die() {
-    this.deathSound.play();
-  }
+    rotate() {
+        this.angle += this.rotCoef * this.turnSpeed * this.dt;
+    }
 
-  update(time) {
-    this.dt = (time - this.lastFrame) / 1024;
-    this.draw();
+    draw() {
+        this.ctx.strokeStyle = "black";
+        this.ctx.fillStyle = this.color;
+        this.ctx.lineWidth = this.size / 20;
+        this.ctx.beginPath();
+        this.ctx.moveTo(
+            this.x + 4 / 3 * this.radius * Math.cos(this.angle),
+            this.y - 4 / 3 * this.radius * Math.sin(this.angle)
+        );
+        this.ctx.lineTo(
+            this.x - this.radius * (2 / 3 * Math.cos(this.angle) + Math.sin(this.angle)),
+            this.y + this.radius * (2 / 3 * Math.sin(this.angle) - Math.cos(this.angle))
+        );
+        this.ctx.lineTo(
+            this.x - this.radius * (2 / 3 * Math.cos(this.angle) - Math.sin(this.angle)),
+            this.y + this.radius * (2 / 3 * Math.sin(this.angle) + Math.cos(this.angle))
+        );
 
-    this.rotCoef !== 0 ? this.rotate() : null;
-    this.moveing ? this.move() : this.decelerate();
+        this.ctx.closePath();
+        this.ctx.stroke();
+        this.ctx.fill();
+    };
 
-    this.x < -this.radius
-      ? (this.x = this.canvas.width - this.radius)
-      : this.x > this.canvas.width - this.radius
-      ? (this.x = +this.radius)
-      : null;
-    this.y < 0 - this.radius
-      ? (this.y = this.canvas.height - this.radius)
-      : this.y > this.canvas.height - this.radius
-      ? (this.y = this.radius)
-      : null;
+    move() {
+        this.movePower.x += this.moveSpeed * Math.cos(this.angle) * this.dt;
+        this.movePower.y -= this.moveSpeed * Math.sin(this.angle) * this.dt;
+        this.x += this.movePower.x;
+        this.y += this.movePower.y;
+        this.moveSound.play();
+    }
 
-    this.bullets.map((el, idx) =>
-      el.x < 0 || el.y < 0 ? this.destroyBullet(idx) : null
-    );
-    this.lastFrame = time;
-  }
+    decelerate() {
+        let friction = .5;
+        this.movePower.x -= friction * this.movePower.x * this.dt;
+        this.movePower.y -= friction * this.movePower.y * this.dt;
+        this.x += this.movePower.x;
+        this.y += this.movePower.y;
+    }
 
-  destroyBullet(i) {
-    this.bullets.splice(i, 1);
-  }
+    fire(time) {
+        if (time - this.timeLastFired > this.fireRate) {
+            this.bullets.push(new Bullet(this.ctx, this.x, this.y, this.angle, this.bulletSize));
+            this.fireSound.play();
+            this.timeLastFired = time;
+        }
+    }
+
+    die() {
+        this.deathSound.play();
+    }
+
+    update(time) {
+        this.dt = (time - this.lastFrame) / 1024;
+        this.draw();
+
+        this.rotCoef !== 0 ? this.rotate() : null;
+        this.moveing ? this.move() : this.decelerate();
+
+        this.x < - this.radius
+            ? this.x = this.canvas.width - this.radius
+            : this.x > this.canvas.width - this.radius
+                ? this.x = +this.radius
+                : null;
+        this.y < 0 - this.radius
+            ? this.y = this.canvas.height - this.radius
+            : this.y > this.canvas.height - this.radius
+                ? this.y = this.radius
+                : null;
+
+        this.bullets.map((el, idx) => (el.x < 0 || el.y < 0) ? this.destroyBullet(idx) : null);
+        this.lastFrame = time;
+    }
+
+    destroyBullet(i) {
+        this.bullets.splice(i, 1);
+    }
 }
-
-export { SpaceShip };
