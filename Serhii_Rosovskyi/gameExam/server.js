22278e4e924d7c3d5f0c8767296634b86fc9441e
Serhii Rosovskyi <30779010+Rosovskyy@users.noreply.github.com>
2019-06-09 10:53:42 +0300

    server added

---
+++ Serhii_Rosovskyi/gameExam/server.js
@@ -0,0 +1,12 @@
+let express = require("express");
+let app = express();
+let path = require("path");
+
+app.use(express.static(path.resolve(__dirname,"assets")));
+app.use(express.static(path.resolve(__dirname,"dist")));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(3000);
+console.log("Server running at http://127.0.0.1:3000/");
