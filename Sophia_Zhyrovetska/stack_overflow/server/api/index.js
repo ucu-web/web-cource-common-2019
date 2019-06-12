a988652456c040346c55c30947f97bdf7c6a14ac
Sophia Zhyrovetska <zhyrovetska@ucu.edu.ua>
2019-06-12 14:15:22 +0300

    Update index.js

--- Sophia_Zhyrovetska/stack_overflow/server/api/index.js
+++ Sophia_Zhyrovetska/stack_overflow/server/api/index.js
@@ -4,7 +4,6 @@ const apiRouter = new Router();
 
 apiRouter.use("/questions", require("./questions"));
 apiRouter.use("/users", require("./users"));
-apiRouter.use("/hot_network_questions", require("./hot_network_questions"));
 apiRouter.use("/blog", require("./blog"));
 
 module.exports = apiRouter;
