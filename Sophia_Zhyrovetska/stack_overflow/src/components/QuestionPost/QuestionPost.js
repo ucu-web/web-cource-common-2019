e19810c1a7032fdc7ffc537c9edca008b1a1ab66
Sophia Zhyrovetska <zhyrovetska@ucu.edu.ua>
2019-06-12 14:27:02 +0300

    Update QuestionPost.js

--- Sophia_Zhyrovetska/stack_overflow/src/components/QuestionPost/QuestionPost.js
+++ Sophia_Zhyrovetska/stack_overflow/src/components/QuestionPost/QuestionPost.js
@@ -1,24 +1,75 @@
-import "./Hot-network-questions.less";
+import "./Question-post.less";
 import BEM from "../../helpers/BEM";
-const b = BEM("Hot-network-questions");
+const b = BEM("Question-post");
 
-const HotNetworkQuestions = data => {
-  const rootEl = document.createElement("li");
-  rootEl.className = b("question");
+const QuestionPost = data => {
+  const rootEl = document.createElement("article");
+  rootEl.className = b({ "tagged-interesting": data.tagged });
 
   rootEl.innerHTML = `
+    <section class="${b("summary")}" >
+      <h3 class="${b("title")}">
+        <a href="#" class="Ordinary-link"
+          >${data.question}</a
+        >
+      </h3>
+      <div class=" ${b("tags")}">
+        ${data.tags
+          .map(tag => `<a href="#" class="Tag ${b("tag")}">${tag}</a>`)
+          .join("")}
+      </div>
+      <footer class=" ${b("started")}">
+        <a href="#" class=" ${b("started-link")}"
+          >asked
+          <time datetime="${data.year}-${data.month}-${data.day}">${
+    data.year
+  }-${data.month}-${data.day}</time>
+        </a>
+        <a href="#" rel="author" class="Ordinary-link">${data.author}</a>
+        <span
+          class="${b("author-reputation-score")}"
+          title="reputation score"
+          >${data.score}</span>
+      </footer>
+    </section>
+
+    <section class="${b("reaction")}">
+      <div class="${b(
+        "counter"
+      )} Question-reaction-counter Question-reaction-counter--votes">
+        <span class="Question-reaction-counter__number" title="0 votes">
+            ${data.votes}
+        </span>
+        <span>votes</span>
+      </div>
       <div
-              class="Favicon Favicon--codegolf ${b("icon")}"
-              title="The Workplace Stack Exchange"
-      ></div>
-      <a
-              href="#"
-              class="Ordinary-link ${b("question-link")}"
-      >${data.question}</a
+        class="${b(
+          "counter"
+        )} Question-reaction-counter Question-reaction-counter--answers Question-reaction-counter--answers--has-no-answer"
       >
+        <span
+          class="Question-reaction-counter__number"
+          title="${data.answers} answers"
+          >${data.answers}</span
+        >
+        <span>answers</span>
+      </div>
+      <div
+        class="${b(
+          "counter"
+        )} Question-reaction-counter Question-reaction-counter--views"
+      >
+        <span class="Question-reaction-counter__number" title="${
+          data.views
+        } views"
+          >${data.views}</span
+        >
+        <span>views</span>
+      </div>
+    </section>
 `;
 
   return rootEl;
 };
 
-export default HotNetworkQuestions;
+export default QuestionPost;
