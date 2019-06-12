007e413cd69bde828ee7dfc47542ebdcdadb7d71
vitaliyvorobyov <vorobyov@ucu.edu.ua>
2019-06-12 13:58:57 +0300

    Refactor git

--- REST_API/server.js
+++ REST_API/server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Helladadso World!"));
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port , () => console.log(`Example post listening port ${port}!`))
+app.listen(port , () => console.log(`Example post listening port ${port}!`));
--- javascript/projectEuler/problem7.js
+++ javascript/projectEuler/problem7.js
@@ -8,7 +8,7 @@ while (counter !== 6) {
 
     number += 1;
 
-    if ((number  7) && ((number % 2 === 0) || (number % 3 === 0) || (number % 5 === 0) || (number % 7 === 0))) {
+    if ((number !== 7) && ((number % 2 === 0) || (number % 3 === 0) || (number % 5 === 0) || (number % 7 === 0))) {
         continue;
     } else {
         primes.push(number);
