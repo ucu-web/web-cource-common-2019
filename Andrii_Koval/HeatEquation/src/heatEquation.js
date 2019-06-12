ea8a9eaf8877c8a367c0ff6948f27c2186661f94
Andrii <andykoval@ucu.edu.ua>
2019-06-12 13:41:07 +0300

    Visualization almost finished

--- Andrii_Koval/HeatEquation/src/heatEquation.js
+++ Andrii_Koval/HeatEquation/src/heatEquation.js
@@ -1,38 +1,46 @@
 import { interpolateSpectral } from "d3-scale-chromatic";
 
 const CELL_SIZE = 5;
+const MAX_TEMP = 100;
 
-export const generatePlate = size =>
-  new Array(size)
-    .fill(0)
-    .map((_, i) =>
-      new Array(size)
-        .fill(0)
-        .map((_, j) =>
-          i === 0 || j === 0 || i === size - 1 || j === size - 1 ? 100 : 0
-        )
-    );
+const range = size => new Array(size).fill(0);
 
-const render = field =>
-  field
-    .reduce((a, b) => [...a, ...b])
-    .map(
-      number =>
-        `<div class="Point" style="background: ${interpolateSpectral(
-          1 - number / 100
-        )}"></div>`
-    )
-    .join("");
+export const generatePlate = (height, width) =>
+  range(height).map(() => range(width).map(() => 0));
 
-const displayOnCanvas = (canvas, field) => {
+const displayOnCanvas = (canvas, field, arrX, arrY) => {
   canvas.height = field.length * CELL_SIZE;
   canvas.width = field[0].length * CELL_SIZE;
   const ctx = canvas.getContext("2d");
 
   map2D((x, y, value) => {
-    ctx.fillStyle = interpolateSpectral(1 - value / 100);
+    const color = value === MAX_TEMP ? field[y][x - 1] : field[y][x];
+    ctx.fillStyle = isInPoly(arrX, arrY, x, y)
+      ? interpolateSpectral(1 - color / MAX_TEMP)
+      : "#ffffff";
     ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
   }, field);
+};
+
+const isInPoly = (arrX, arrY, x, y) => {
+  let j = arrX.length - 1;
+  let flag = false;
+
+  for (let i = 0; i < arrX.length; i++) {
+    if (
+      ((arrY[i] < y && arrY[j] >= y) || (arrY[j] < y && arrY[i] >= y)) &&
+      (arrX[i] <= x || arrX[j] <= x)
+    ) {
+      if (
+        arrX[i] + ((y - arrY[i]) / (arrY[j] - arrY[i])) * (arrX[j] - arrX[i]) <
+        x
+      ) {
+        flag = !flag;
+      }
+    }
+    j = i;
+  }
+  return flag;
 };
 
 const copyArray = arr => arr.map(e => (e instanceof Array ? copyArray(e) : e));
@@ -40,25 +48,60 @@ const copyArray = arr => arr.map(e => (e instanceof Array ? copyArray(e) : e));
 const map2D = (func, arr) =>
   arr.map((innerArr, y) => innerArr.map((value, x) => func(x, y, value)));
 
+const onClick = (e, field) => {
+  const X = Math.floor(e.offsetX / CELL_SIZE);
+  const Y = Math.floor(e.offsetY / CELL_SIZE);
+  field[Y][X] = MAX_TEMP;
+};
+
+export const addCoordinate = (e, polyX, polyY) => {
+  const X = Math.floor(e.offsetX / CELL_SIZE);
+  const Y = Math.floor(e.offsetY / CELL_SIZE);
+  polyX.push(X);
+  polyY.push(Y);
+};
+
 export const visualize = (field, temp, c, dt, dx) => {
-  // const container = document.getElementById("Visualization");
+  const canvas = document.querySelector("#canvas");
+  const button = document.getElementsByClassName("render-button")[0];
+  const polyX = [];
+  const polyY = [];
+  const event = e => addCoordinate(e, polyX, polyY);
+  canvas.addEventListener("click", event);
+
+  button.addEventListener("click", e => {
+    e.preventDefault();
+    canvas.removeEventListener("click", event);
+    canvas.addEventListener("click", e => onClick(e, field));
+  });
+
   setInterval(() => {
     const isMarginal = (x, y) =>
-      x === 0 || y === 0 || x === field[0].length - 1 || y === field.length - 1;
+      field[x][y] === MAX_TEMP ||
+      x === 0 ||
+      y === 0 ||
+      x === field[0].length - 1 ||
+      y === field.length - 1;
 
     const getSideValuesSum = (x, y) =>
-      field[x + 1][y] + field[x - 1][y] + field[x][y + 1] + field[x][y - 1];
+      field[x + 1][y] +
+      field[x - 1][y] +
+      field[x][y + 1] +
+      field[x][y - 1] +
+      field[x + 1][y + 1] +
+      field[x - 1][y - 1] +
+      field[x - 1][y + 1] +
+      field[x + 1][y - 1];
 
-    const calculateNewValue = (x, y, value) =>
-      isMarginal(x, y)
-        ? value
-        : value + ((c * dt) / dx ** 2) * (getSideValuesSum(x, y) - 4 * value);
-
+    const calculateNewValue = (y, x, value) =>
+      isInPoly(polyX, polyY, y, x)
+        ? isMarginal(x, y)
+          ? value
+          : value + ((c * dt) / dx ** 2) * (getSideValuesSum(x, y) - 8 * value)
+        : 1;
     field = map2D(calculateNewValue, field);
 
     const canvas = document.querySelector("#canvas");
-    displayOnCanvas(canvas, field);
-
-    // container.innerHTML = render(field);
+    displayOnCanvas(canvas, field, polyX, polyY);
   }, 1);
 };
--- Andrii_Koval/HeatEquation/src/index.js
+++ Andrii_Koval/HeatEquation/src/index.js
@@ -1,8 +1,11 @@
 import {visualize, generatePlate} from "./heatEquation";
 
-const plate1 = generatePlate(100);
-const plate2 = generatePlate(100);
+const plate1 = generatePlate(100, 100);
+const plate2 = generatePlate(100, 100);
 const c = 80.4 / (7800 * 0.46);
-const dt = 10;
-const dx = 2;
+const dt = 7;
+const dx = 1;
+
+
+
 visualize(plate1, plate2, c, dt, dx);
