b24d2c7346509a02c478319cd2c55c2fe340f44c
shtokhman <shtokhman.y@gmail.com>
2019-06-23 14:14:02 +0300

    Removed odd files

--- Roman_Milishchuk/JsProblems/EulerProject/1.js
+++
@@ -1,7 +0,0 @@
-let sum = 0;
-for (let i = 0; i < 1000; i++) {
-    if (i % 5 === 0 || i % 3 === 0) {
-        sum += i;
-    }
-}
-console.log(sum);
--- Roman_Milishchuk/JsProblems/EulerProject/6.js
+++
@@ -1,7 +0,0 @@
-let res = 0;
-for (let i = 1; i < 101; i++) {
-    for (let j = i + 1; j < 101; j++) {
-        res += 2 * i * j;
-    }
-}
-console.log(res);
